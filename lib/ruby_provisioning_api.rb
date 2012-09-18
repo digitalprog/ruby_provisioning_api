81151dde751c560eed28c4ebf1a87658bffa2fad
Damiano Braga <damiano.braga@gmail.com>
2012-09-18 17:42:52 +0200

    Fixed load configuration

--- lib/ruby_provisioning_api.rb
+++ lib/ruby_provisioning_api.rb
@@ -1,14 +1,42 @@
 require 'ruby_provisioning_api/version'
 require 'ruby_provisioning_api/connection'
+require './lib/ruby_provisioning_api/configuration'
+
 require 'faraday'
+require 'yaml'
 
 module RubyProvisioningApi
   class << self
 
+    attr_accessor :configuration
+    
+    attr_reader :root_path
+    attr_reader :lib_path
+
     def new
-      RubyProvisioningApi::Connection.new()
+      puts "Ehi!"
+      RubyProvisioningApi::Connection.new
     end
+
+    # private
+    def root_path
+					@root_path = File.expand_path "..", __FILE__
+    end
+		  
+		  def lib_path
+			  @lib_path = File.expand_path "../ruby_provisioning_api", __FILE__
+			 end
+
+    # Adds configuration ability to the gem
+    def configuration
+      @configuration ||= RubyProvisioningApi::Configuration.new
+    end
+
+    def configure
+      yield(configuration)
+    end
+ 
+
+
   end
 end
-
-RubyProvisioningApi.new
--- lib/ruby_provisioning_api/connection.rb
+++ lib/ruby_provisioning_api/connection.rb
@@ -1,7 +1,23 @@
 module RubyProvisioningApi
   class Connection
-    def Initialized
-      puts "Initialized"
+
+    attr_reader :token
+
+    def initialize
+      if File.exist?(RubyProvisioningApi.configuration.config_file)
+        @GAPPS = YAML.load_file(RubyProvisioningApi.configuration.config_file)
+      else
+        raise "RubyProvisioningApi: File #{RubyProvisioningApi.configuration.config_file} not found, maybe you forgot to define it ?"
+      end
+
+      client = Faraday.new(:url => 'https://www.google.com') do |faraday|
+        faraday.request  :url_encoded             # form-encode POST params
+        faraday.response :logger                  # log requests to STDOUT
+        faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+      end
+      response = client.post '/accounts/ClientLogin', {:Email => GAPPS[:email], :Passwd => GAPPS[:password], :accountType =>  "HOSTED", :service => "apps"}
+      @token = response.body.split("\n").last.split('Auth=').last
     end
+
   end
 end
