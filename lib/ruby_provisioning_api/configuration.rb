5f996afbc1ddb72f74c618009a473b4ae23e275b
Damiano <damiano@damiano-945P-S3.(none)>
2012-09-19 15:36:30 +0200

    Added configuration

--- lib/ruby_provisioning_api/configuration.rb
+++ lib/ruby_provisioning_api/configuration.rb
@@ -2,14 +2,19 @@ module RubyProvisioningApi
 
   class Configuration
 
-    attr_accessor :config_file
+    attr_accessor :config_file, :config
 
     def initialize
     		# For rails env
       # @ldap_config_file = "#{Rails.root}/config/google_apps.yml"
-      # Temporary patch. Insert your config file here.
-      @config_file = "YOUR_PERSONAL_DIRECTORY/google_apps.yml"
-
+      # Temporary patch. Insert your config directory here.
+      config_file = "PATH/lib/ruby_provisioning_api/config/google_apps.yml"
+      # Get necessary data from configuration files
+      if File.exist?(config_file)
+        @config = YAML.load_file(config_file)
+      else
+        raise "RubyProvisioningApi: File #{config_file} not found, maybe you forgot to define it ?"
+      end
     end
 
   end
