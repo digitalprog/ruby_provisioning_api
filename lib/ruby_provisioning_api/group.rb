04e5ba783098bf7a14e962c41403fd3350a77841
Damiano <damiano@damiano-945P-S3.(none)>
2012-09-20 09:55:56 +0200

    Added update and delete methods

--- lib/ruby_provisioning_api/group.rb
+++ lib/ruby_provisioning_api/group.rb
@@ -8,6 +8,8 @@ module RubyProvisioningApi
 
     ACTIONS = { 
       :create =>  { method: "POST" , url: "#{GROUP_PATH}"},
+      :update =>  { method: "PUT" , url: "#{GROUP_PATH}/groupId"},
+      :delete =>  { method: "DELETE" , url: "#{GROUP_PATH}/groupId"},
       :retrieve_all => { method: "GET" , url: "#{GROUP_PATH}" },
       :retrieve => { method: "GET" , url: "#{GROUP_PATH}/groupId" }
     }
@@ -27,11 +29,12 @@ module RubyProvisioningApi
       response = perform(ACTIONS[:retrieve_all])
     end
 
+   # Save(Create) a group POST https://apps-apis.google.com/a/feeds/group/2.0/domain
     def save
       Group.create(group_id, group_name, description, email_permission)
     end
 
-    #Create POST https://apps-apis.google.com/a/feeds/group/2.0/domain
+    # Create a group POST https://apps-apis.google.com/a/feeds/group/2.0/domain
     def self.create(group_id, group_name, description, email_permission)
       builder = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
         xml.send(:'atom:entry', 'xmlns:atom' => 'http://www.w3.org/2005/Atom', 'xmlns:apps' => 'http://schemas.google.com/apps/2006') {
@@ -46,8 +49,7 @@ module RubyProvisioningApi
       #TODO controllare eventuali eccezioni e restituire true/false in caso      
     end
 
-
-    #Retrieve a group  GET https://apps-apis.google.com/a/feeds/group/2.0/domain/groupId
+    # Retrieve a group  GET https://apps-apis.google.com/a/feeds/group/2.0/domain/groupId
     def self.find(group_id)
       # Creating a deep copy of ACTION object
       params = Marshal.load(Marshal.dump(ACTIONS[:retrieve]))
@@ -57,12 +59,31 @@ module RubyProvisioningApi
     end
 
 
-    #Update a group                   PUT https://apps-apis.google.com/a/feeds/group/2.0/domain/groupId
-    def self.update
+    # Update group information   PUT https://apps-apis.google.com/a/feeds/group/2.0/domain/groupId
+    def update(current_group_id)
+      # Creating a deep copy of ACTION object
+      params = Marshal.load(Marshal.dump(ACTIONS[:update]))
+      # Replacing place holder groupId with correct group_id
+      params[:url].gsub!("groupId",current_group_id)
+
+      builder = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
+        xml.send(:'atom:entry', 'xmlns:atom' => 'http://www.w3.org/2005/Atom', 'xmlns:apps' => 'http://schemas.google.com/apps/2006') {
+          xml.send(:'atom:category', 'scheme' => 'http://schemas.google.com/g/2005#kind', 'term' => 'http://schemas.google.com/apps/2006#emailList')
+          xml.send(:'apps:property', 'name' => 'groupName', 'value' => group_name)
+          xml.send(:'apps:property', 'name' => 'description', 'value' => description)
+          xml.send(:'apps:property', 'name' => 'emailPermission', 'value' => email_permission)
+        }
+      end 
+      response = Entity.perform(params,builder.to_xml)     
     end
 
-    #Delete group                  DELETE https://apps-apis.google.com/a/feeds/group/2.0/domain/groupId
-    def self.delete
+    #Delete group  DELETE https://apps-apis.google.com/a/feeds/group/2.0/domain/groupId
+    def self.delete(group_id)
+      # Creating a deep copy of ACTION object
+      params = Marshal.load(Marshal.dump(ACTIONS[:delete]))
+      # Replacing place holder groupId with correct group_id
+      params[:url].gsub!("groupId",group_id)
+      response = perform(params)
     end
 
     # TODO
