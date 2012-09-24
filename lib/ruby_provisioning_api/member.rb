92e79ca66ad623b7e6c632ac97f8f648afa6a093
Damiano Braga <damiano.braga@gmail.com>
2012-09-24 20:40:46 +0200

    Added method members

--- lib/ruby_provisioning_api/member.rb
+++ lib/ruby_provisioning_api/member.rb
@@ -3,9 +3,10 @@ module RubyProvisioningApi
   module Member
     
     GROUP_PATH = "/group/2.0/#{RubyProvisioningApi.configuration[:domain]}"
-
+    USER_ATTRIBUTES = ['userName','password','suspended','quota','familyName','givenName']
     ACTIONS = {
         :member => {method: "GET", url: "#{GROUP_PATH}/groupId/member/memberId"},
+        :members => {method: "GET", url: "#{GROUP_PATH}/groupId/member"},
     }
 
     def self.included(base)
@@ -21,11 +22,31 @@ module RubyProvisioningApi
       # super(member_id)
     # end
 
-    def members(group_id)
-      all
+    # Retrieve all users which are members for a group
+    # @see https://developers.google.com/google-apps/provisioning/#retrieving_all_members_of_a_group GET https://apps-apis.google.com/a/feeds/group/2.0/domain/groupId/member[?[start=]&[includeSuspendedUsers=true|false]] 
+    # @return [Array<User>] all users which are members for a group
+    def members
+      # Creating a deep copy of ACTION object
+      params = Marshal.load(Marshal.dump(ACTIONS[:members]))
+      # Replacing placeholder groupId with correct group_id
+      params[:url].gsub!("groupId",group_id)
+      # Perform the request & Check if the response contains an error
+      # TODO: missing check_response because it's redundant
+      # Parse the response
+      xml = Nokogiri::XML(self.class.perform(params).body)
+      # Prepare a User array
+      users = []
+      xml.children.css("entry").each do |entry|
+        # If the member is a user
+        if entry.css("apps|property[name]")[0].attributes["value"].value.eql?("User")
+          # Get the user_name
+          user_name = entry.css("apps|property[name]")[1].attributes["value"].value.split("@")[0]
+          users << User.find(user_name)
+        end
+      end
+      # Return the array of users (members)
+      users      
     end
-
-
 
   end
 end
