4bd380ab9c877213982c3b6b7dc90b5391965483
Damiano Braga <damiano.braga@gmail.com>
2012-09-29 13:30:50 +0200

    Added spec helper configuration with spork

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,22 @@
 require 'rubygems'
-require 'bundler/setup'
+require 'spork'
+#uncomment the following line to use spork with the debugger
+#require 'spork/ext/ruby-debug'
 
+Spork.prefork do
+  # Loading more in this block will cause your tests to run faster. However,
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
+end
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+
+end
+
+require 'rubygems'
+require 'bundler/setup'
+require 'rspec'
 require 'ruby_provisioning_api'
 
 RSpec.configure do |config|
