a79fa2790671767b12d5cbce7f4e0613a4c2dd97
Damiano Braga <damiano.braga@gmail.com>
2012-10-02 11:12:02 +0200

    Updated #21

--- lib/ruby_provisioning_api/group.rb
+++ lib/ruby_provisioning_api/group.rb
@@ -329,7 +329,7 @@ module RubyProvisioningApi
     end
 
     def add_entity(entity,entity_id)
-      user = User.find(entity_id)
+      # user = User.find(entity_id)
       # Creating the XML request
       builder = prepare_xml_request("#{entity}_id".to_sym => entity_id)
       params = self.class.prepare_params_for("add_#{entity}".to_sym, "groupId" => group_id )
