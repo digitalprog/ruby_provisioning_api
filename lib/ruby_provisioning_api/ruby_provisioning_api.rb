8a505b50cfb2e720c61ab324ac35b94188ac3745
Davide Targa <davide.targa@gmail.com>
2012-10-03 14:01:04 +0200

    More comments on ruby provisionin api module.

--- lib/ruby_provisioning_api/ruby_provisioning_api.rb
+++ lib/ruby_provisioning_api/ruby_provisioning_api.rb
@@ -1,14 +1,33 @@
+# This is the main RbuyProvisioningApi module.
+#
 module RubyProvisioningApi
   class << self
 
     attr_writer :configuration
     attr_reader :connection
 
-    # Adds configuration ability to the gem
+    # Returns the current configuration of the gem
+    #
+    # @example Retrieve the configuration of the RubyProvisioningApi gem
+    #   RubyProvisioningApi.configuration # => [Configuration]
+    #
+    # @return [Configuration]
+    #
     def configuration
       @configuration ||= RubyProvisioningApi::Configuration.new
     end
 
+    # Adds configuration ability to the gem
+    #
+    # @example Ruby on rials initializer for the Ruby Provisioning Api gem
+    #   RubyProvisioningApi.configure do |config|
+    #     config.config_file = "/path/to/file.yml"
+    #     config.ca_file = "/path/to/ca_file"
+    #   end
+    #
+    # It's possible to write an initializer to customize the behaviour of the gem. For a list of all the available
+    # options see the {Configuration} class
+    #
     def configure
       yield(configuration)
     end
