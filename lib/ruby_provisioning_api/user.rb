c62b2d793cf5606a01330785dc54bed114cd7b15
Davide Targa <davide.targa@gmail.com>
2012-10-15 16:31:40 +0200

    Fix specs.

--- lib/ruby_provisioning_api/user.rb
+++ lib/ruby_provisioning_api/user.rb
@@ -21,11 +21,8 @@ module RubyProvisioningApi
     validates :user_name, :family_name, :given_name, :presence => true
 
     def user_name=(value)
-      unless value == user_name
+      unless value == @user_name
         user_name_will_change!
-
-        puts "\n\nchanging username"
-        puts "#{user_name} -> #{value}"
       end
       @user_name = value
     end
