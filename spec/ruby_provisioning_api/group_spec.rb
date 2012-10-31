5f148e79aea57c813e74eb37224198068cb07e70
Davide Targa <davide.targa@gmail.com>
2012-10-31 11:29:09 +0100

    More specs on groups.

--- spec/ruby_provisioning_api/group_spec.rb
+++ spec/ruby_provisioning_api/group_spec.rb
@@ -82,7 +82,7 @@ describe RubyProvisioningApi::Group do
 
     context "with existing groups" do
 
-      before do
+      before :all do
         VCR.use_cassette "all-retrieve-all-groups" do
           @groups = RubyProvisioningApi::Group.all
         end
@@ -91,14 +91,32 @@ describe RubyProvisioningApi::Group do
       it "should retrieve all the groups in the domain" do
         @groups.length.should be(2)
       end
-      it "should return an Array"
-      it "should return an Array of Group"
+
+      it "should return an Array" do
+        @groups.should be_kind_of(Array)
+      end
+
+      it "should return an Array of Group" do
+        @groups.each do |group|
+          group.should be_kind_of(RubyProvisioningApi::Group)
+        end
+      end
 
     end
 
     context "with no existing groups" do
 
-      it "should return an empty array"
+      before do
+        VCR.use_cassette "all-delete_all_and_find_all" do
+          RubyProvisioningApi::Group.all.each{ |group| group.delete }
+          @groups = RubyProvisioningApi::Group.all
+        end
+      end
+
+      it "should return an empty array" do
+        @groups.should be_kind_of(Array)
+        @groups.should be_empty
+      end
 
     end
 
@@ -106,9 +124,28 @@ describe RubyProvisioningApi::Group do
 
   describe "#save" do
 
-    it "should return true when saving a valid Group"
-    it "should save a new record if the group_id does not exist and return true"
-    it "should return false when saving a non valid Group"
+    before :all do
+      @group = RubyProvisioningApi::Group.new(:group_id => "foobarz", :group_name => "foobar2_name", :description => "foobar_description", :email_permission => "Owner")
+      VCR.use_cassette "save-saving_a_valid_group" do
+        @groups_before = RubyProvisioningApi::Group.all
+        @retval = @group.save
+        @groups_after = RubyProvisioningApi::Group.all
+      end
+    end
+
+    it "should return true when saving a valid Group" do
+      @retval.should be(true)
+    end
+
+
+    it "should save a new record if the group_id does not exist" do
+      @groups_before.length.should be(@groups_after.length - 1)
+    end
+
+    it "should return false when saving a non valid Group" do
+      invalid_group = RubyProvisioningApi::Group.new(:group_id => "oo", :group_name => "", :description => "")
+      invalid_group.save.should be(false)
+    end
 
     context "on update" do
 
